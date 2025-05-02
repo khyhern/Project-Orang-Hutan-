7f62feac6d6bcb116e7ac42ca7c405cbf6f8c792
hahsguv <rexyow0624@gmail.com>
2025-05-02 22:11:54 +0800

    DialogueBox_base(D)

--- Assets/Scripts/Dialogue/DialogueUI.cs
+++ Assets/Scripts/Dialogue/DialogueUI.cs
@@ -5,6 +5,7 @@ using TMPro;
 
 public class DialogueUI : MonoBehaviour
 {
+	[SerializeField] private GameObject dialogueBox;
     [SerializeField] private TMP_Text textLabel;
 	[SerializeField] private DialogueObject testDialogue;
 	
@@ -13,11 +14,13 @@ public class DialogueUI : MonoBehaviour
     private void Start()
     {
         typewriterEffect = GetComponent<TypeWriterEffect>();
+		CloseDialogueBox();
 		ShowDialogue(testDialogue);
     }
 	
 	public void ShowDialogue(DialogueObject dialogueObject)
 	{
+		dialogueBox.SetActive(true);
 		StartCoroutine(StepThroughDialogue(dialogueObject));
 	}
 	
@@ -30,5 +33,13 @@ public class DialogueUI : MonoBehaviour
 			yield return typewriterEffect.Run(dialogue, textLabel);
 			yield return new WaitUntil(()=> Input.GetKeyDown(KeyCode.Space));
 		}
+		
+		CloseDialogueBox();
+	}
+	
+	private void CloseDialogueBox()
+	{
+		dialogueBox.SetActive(false);
+		textLabel.text = string.Empty;
 	}
 }
