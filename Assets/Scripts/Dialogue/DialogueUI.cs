55d6036ef6aa99de5d12f9b8b7ff3f62696c3acd
hahsguv <rexyow0624@gmail.com>
2025-05-02 15:01:53 +0800

    Dialogue3

--- Assets/Scripts/Dialogue/DialogueUI.cs
+++ Assets/Scripts/Dialogue/DialogueUI.cs
@@ -6,9 +6,29 @@ using TMPro;
 public class DialogueUI : MonoBehaviour
 {
     [SerializeField] private TMP_Text textLabel;
+	[SerializeField] private DialogueObject testDialogue;
+	
+	private TypeWriterEffect typewriterEffect;
 
     private void Start()
     {
-        GetComponent<TypeWriterEffect>().Run("This is a bit of text!\nHello.", textLabel);
+        typewriterEffect = GetComponent<TypeWriterEffect>();
+		ShowDialogue(testDialogue);
     }
+	
+	public void ShowDialogue(DialogueObject dialogueObject)
+	{
+		StartCoroutine(StepThroughDialogue(dialogueObject));
+	}
+	
+	private IEnumerator StepThroughDialogue(DialogueObject dialogueObject)
+	{
+		yield return new WaitForSeconds(3);
+		
+		foreach (string dialogue in dialogueObject.Dialogue)
+		{
+			yield return typewriterEffect.Run(dialogue, textLabel);
+			yield return new WaitUntil(()=> Input.GetKeyDown(KeyCode.Space));
+		}
+	}
 }
--- Assets/Scripts/Dialogue/TypeWriterEffect.cs
+++ Assets/Scripts/Dialogue/TypeWriterEffect.cs
@@ -5,20 +5,24 @@ using TMPro;
 
 public class TypeWriterEffect : MonoBehaviour
 {
-// Start is called before the first frame update
-    public void Run(string textToType, TMP_Text textLabel)
+	[SerializeField]private float typewriterSpeed = 50f;
+	
+	// Start is called before the first frame update
+    public Coroutine Run(string textToType, TMP_Text textLabel)
     {
-        StartCoroutine(TypeText(textToType, textLabel));
+        return StartCoroutine(TypeText(textToType, textLabel));
     }
     // Update is called once per frame
     private IEnumerator TypeText(string textToType, TMP_Text textLabel)
     {
+		textLabel.text = string.Empty;
+		
         float t = 0;
         int charIndex = 0;
         
         while (charIndex < textToType.Length)
         {
-            t += Time.deltaTime;
+            t += Time.deltaTime * typewriterSpeed;
             charIndex = Mathf. FloorToInt(t);
             charIndex = Mathf.Clamp(charIndex, 0, textToType.Length);
             
