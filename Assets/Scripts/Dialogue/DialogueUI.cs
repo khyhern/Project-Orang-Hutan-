7166be2f2d1415592c877b80a8e2f0b192caae7a
hahsguv <rexyow0624@gmail.com>
2025-05-02 12:48:38 +0800

    Dialogue 2

---
+++ Assets/Scripts/Dialogue/DialogueUI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueUI : MonoBehaviour
+{
+    [SerializeField] private TMP_Text textLabel;
+
+    private void Start()
+    {
+        GetComponent<TypeWriterEffect>().Run("This is a bit of text!\nHello.", textLabel);
+    }
+}
