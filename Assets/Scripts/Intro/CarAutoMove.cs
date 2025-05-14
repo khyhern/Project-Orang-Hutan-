47f66774900849e5e16c4cbe43f25eef413df65c
hahsguv <rexyow0624@gmail.com>
2025-05-14 15:43:37 +0800

    done1

--- Assets/Scripts/Intro/CarAutoMove.cs
+++ Assets/Scripts/Intro/CarAutoMove.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class CarAutoMove : MonoBehaviour
 {
     public float speed = 20f;
+    private bool shouldMove = true;
 
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        if (shouldMove)
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Stopper"))
+        {
+            shouldMove = false;
+        }
     }
 }
