0a16da662c6a86c74584d64e054399466c9cbd3b
hahsguv <rexyow0624@gmail.com>
2025-05-18 18:00:05 +0800

    movement done

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private InputManager  inputManager;
+    
+    [SerializeField]
+    private float playerSpeed = 2.0f;
+    [SerializeField]
+    private float jumpHeight = 1.0f ;
+    [SerializeField]
+    private float gravityValue = -9.81f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        inputManager = InputManager.Instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }            
+        
+        Vector3 movement = inputManager.GetPlayerMovement();
+        Vector3 move = new Vector3(movement.x, 0f, movement.y);
+        controller.Move(move * Time.deltaTime * playerSpeed);
+        
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }    
+
+        if (inputManager.PlayerJumpedThisFrame() && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);    
+        }
+        
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
