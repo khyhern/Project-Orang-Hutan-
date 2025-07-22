e7b996e7f0cce8f7200ca81679066c34fb2746a2
hahsguv <rexyow0624@gmail.com>
2025-07-22 14:39:43 +0800

    audio mixer & main Menu

---
+++ Assets/Scripts/Manager/BtnManager.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class BtnManager : MonoBehaviour
+{	
+
+	public GameObject PauseMenu;
+	public static bool isPaused;
+	
+	public AudioMixer mixer;
+	
+	[SerializeField] private GameObject MainPanel;
+	[SerializeField] private GameObject OptionPanel;
+	
+	//Main Menu Buttons
+	
+	public void PlayButton() 
+	{
+		SceneManager.LoadScene(""); //change to select save file / Level selection
+	}
+	
+	public void OptionButton() 
+	{
+		MainPanel.SetActive(false);
+		OptionPanel.SetActive(true);
+	}
+	
+	public void OptionBackButton() 
+	{
+		MainPanel.SetActive(true);
+		OptionPanel.SetActive(false);
+	}
+    public void QuitGameButton()
+    {
+        Application.Quit(); // For quitting the application when built
+    }
+
+    // Restart the level
+    public void RestartButton()
+    {
+        SceneManager.LoadScene(""); // Replace "level 0" with your actual scene name
+    }
+    
+    public void UpdateBGMVolume(float volume)
+    {
+	    mixer.SetFloat("BGMVolume", volume);
+    }
+    
+    public void UpdateSFXVolume(float volume)
+    {
+	    mixer.SetFloat("SFXVolume", volume);
+    }
+    
+    public void SaveVolume()
+    {
+	    mixer.GetFloat("BGMVolume", out float bgmVolume);
+	    PlayerPrefs.SetFloat("BGMVolume", bgmVolume);
+	    
+	    mixer.GetFloat("SFXVolume", out float sfxVolume);
+	    PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+    }
+
+    // Go to the main menu
+    public void MainMenuButton()
+    {
+		PauseMenu.SetActive(false);
+		Time.timeScale = 1f;
+		isPaused = false;
+		SceneManager.LoadScene("Main Menu");
+    }
+}
