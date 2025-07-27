57fee74b86d74723c45b1248f2c05537fbce9abd
BingChiau <118031847+bingchiau@users.noreply.github.com>
2025-07-27 20:14:03 +0800

    fix everything

--- Assets/Scripts/Puzzle/PickupItem.cs
+++ Assets/Scripts/Puzzle/PickupItem.cs
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 using UnityEngine;
-=======
-﻿using UnityEngine;
->>>>>>> parent of da63deb (organize scripts)
 
 [RequireComponent(typeof(Collider))]
 public class PickupItem : MonoBehaviour, IInteractable
 {
-<<<<<<< HEAD
     [Tooltip("The data representing this physical puzzle item.")]
     public PuzzleItemData itemData;
 
@@ -31,22 +26,3 @@ public class PickupItem : MonoBehaviour, IInteractable
         Destroy(gameObject); // Will trigger PuzzleSlotReference.OnDestroy if attached
     }
 }
-=======
-    [Tooltip("Data representing this physical puzzle item.")]
-    public PuzzleItemData itemData;
-
-    [Tooltip("Which interaction group this pickup belongs to.")]
-    [SerializeField] private InteractionGroup interactionGroup = InteractionGroup.Pickup;
-
-    public void Interact()
-    {
-        if (itemData == null || InventorySystem.Instance == null) return;
-
-        InventorySystem.Instance.PickUp(itemData);
-        Debug.Log($"[PickupItem] Picked up: {itemData.itemName}");
-        Destroy(gameObject);
-    }
-
-    public InteractionGroup GetInteractionGroup() => interactionGroup;
-}
->>>>>>> parent of da63deb (organize scripts)
