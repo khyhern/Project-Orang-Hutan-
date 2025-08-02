fc1adc220c888f3bd6cb21ec4a4993e3c4d2577e
hahsguv <rexyow0624@gmail.com>
2025-08-02 21:43:12 +0800

    fix oje

--- Assets/Scripts/Manager/BtnManager.cs
+++ Assets/Scripts/Manager/BtnManager.cs
@@ -1,65 +1,67 @@
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class BtnManager : MonoBehaviour
 {	
 
 	public GameObject PauseMenu;
-	public GameObject DeletePanel;
 	public static bool isPaused;
 	
+	public AudioMixer mixer;
+	
+	[SerializeField] private GameObject MainPanel;
+	[SerializeField] private GameObject OptionPanel;
 	
 	public void PlayButton() 
 	{
-		SceneManager.LoadScene(""); 
+		SceneManager.LoadScene(""); //change to select save file / Level selection
 	}
 	
-	public void Level1Button() 
+	public void OptionButton() 
 	{
-		SceneManager.LoadScene("Level 1");
+		MainPanel.SetActive(false);
+		OptionPanel.SetActive(true);
 	}
 	
-	public void Level2Button() 
-	{
-		SceneManager.LoadScene("Level 2");
-	}
-	
-	public void Level3Button() 
-	{
-		SceneManager.LoadScene("Level 3");
-	}
-
     public void QuitGameButton()
     {
-        Application.Quit(); 
+        Application.Quit(); // For quitting the application when built
     }
     
-    public void DeleteButton()
-    {
-	    DeletePanel.SetActive(true);
-    }
-    
-    public void TrueDeleteButton()
-    {
-	   
-    }
-    public void DeleteBackButton()
-    {
-	    DeletePanel.SetActive(false);
-    }
-
-
     public void RestartButton()
     {
-        SceneManager.LoadScene(""); 
+        SceneManager.LoadScene(""); // Replace "level 0" with your actual scene name
     }
 
-
+       
+    public void UpdateBGMVolume(float volume)
+    {
+	    mixer.SetFloat("BGMVolume", volume);
+    }
+    
+    public void UpdateSFXVolume(float volume)
+    {
+	    mixer.SetFloat("SFXVolume", volume);
+    }
+    
+    public void SaveVolume()
+    {
+	    mixer.GetFloat("BGMVolume", out float bgmVolume);
+	    PlayerPrefs.SetFloat("BGMVolume", bgmVolume);
+	    
+	    mixer.GetFloat("SFXVolume", out float sfxVolume);
+	    PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+    }
+    
+    // Go to the main menu
     public void MainMenuButton()
     {
 		PauseMenu.SetActive(false);
 		Time.timeScale = 1f;
 		isPaused = false;
-		SceneManager.LoadScene("Main Menu");
+		SceneManager.LoadScene("MainMenu");
     }
 }
